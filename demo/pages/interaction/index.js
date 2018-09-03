009c5cebd3fc39ef30521cee58778521d3445078
王自强 <814120507@qq.com>
2018-09-03 18:50:09 +0800

    添加柱状图交互

--- demo/pages/interaction/index.js
+++ demo/pages/interaction/index.js
@@ -1,9 +1,15 @@
 const PieSelect = () => import('./pie-select');
+const IntervalSelect = () => import('./interval-select');
 
 export default [
   {
     path: 'interaction/pie-select',
     meta: { name: '饼图选中交互' },
     component: PieSelect,
+  },
+  {
+    path: 'interaction/interval-select',
+    meta: { name: '柱状图选中' },
+    component: IntervalSelect,
   },
 ];
---
+++ demo/pages/interaction/interval-select.vue
@@ -0,0 +1,90 @@
+<template>
+  <vf-chart :data="data" @ready="handleReady">
+    <vf-interval position="year*sales"/>
+    <vf-scale field="sales" tick-count="5"/>
+    <vf-tooltip disabled/>
+    <vf-interaction type="interval-select"
+      :select-axis-style="{
+        fill: '#000',
+        fontWeight: 'bold'
+      }"
+      :default-selected="{
+        year: '1962 年',
+        sales: 38
+      }"
+      mode="range"
+      @end="handleEnd"
+      />
+  </vf-chart>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [{
+        year: '1951 年',
+        sales: 38,
+      }, {
+        year: '1952 年',
+        sales: 52,
+      }, {
+        year: '1956 年',
+        sales: 61,
+      }, {
+        year: '1957 年',
+        sales: 145,
+      }, {
+        year: '1958 年',
+        sales: 48,
+      }, {
+        year: '1959 年',
+        sales: 38,
+      }, {
+        year: '1960 年',
+        sales: 38,
+      }, {
+        year: '1962 年',
+        sales: 38,
+      }],
+    };
+  },
+  methods: {
+    handleReady(chart) {
+      this.canvas = chart.get('canvas');
+      const group = this.canvas.addGroup();
+      this.shapes = {};
+      this.data.forEach((obj) => {
+        const point = chart.getPosition(obj);
+        const text = group.addShape('text', {
+          attrs: {
+            x: point.x,
+            y: point.y - 5,
+            text: obj.sales,
+            textAlign: 'center',
+            textBaseline: 'bottom',
+            fill: '#808080',
+          },
+        });
+        this.shapes[obj.year] = text;
+      });
+    },
+    handleEnd(e) {
+      const { data, selected } = e;
+      this.lastTextShape && this.lastTextShape.attr({
+        fill: '#808080',
+        fontWeight: 'normal',
+      });
+      if (selected) {
+        const textShape = this.shapes[data.year];
+        textShape.attr({
+          fill: '#000',
+          fontWeight: 'bold',
+        });
+        this.lastTextShape = textShape;
+      }
+      this.canvas.draw();
+    },
+  },
+};
+</script>
