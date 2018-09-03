36b4d17a641e679d5b9ca405a35503eebc6f0070
王自强 <814120507@qq.com>
2018-09-03 15:36:23 +0800

    添加bar——分组柱状图

--- demo/pages/bar/index.js
+++ demo/pages/bar/index.js
@@ -1,9 +1,15 @@
 import BasicColumn from './basic-column';
+import DodgeColumn from './dodge-column';
 
 export default [
   {
     path: 'bar/basic-column',
     meta: { name: '基础柱状图图' },
     component: BasicColumn,
+  },
+  {
+    path: 'bar/dodge-column',
+    meta: { name: '分组柱状图图' },
+    component: DodgeColumn,
   },
 ];
