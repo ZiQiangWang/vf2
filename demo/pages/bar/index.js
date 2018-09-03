d90a71ff9419e0b742a15f7a4e92427c189b1296
王自强 <814120507@qq.com>
2018-09-03 15:38:19 +0800

    添加bar——层叠柱状图

--- demo/pages/bar/index.js
+++ demo/pages/bar/index.js
@@ -1,15 +1,21 @@
 import BasicColumn from './basic-column';
 import DodgeColumn from './dodge-column';
+import StackColumn from './stack-column';
 
 export default [
   {
     path: 'bar/basic-column',
-    meta: { name: '基础柱状图图' },
+    meta: { name: '基础柱状图' },
     component: BasicColumn,
   },
   {
     path: 'bar/dodge-column',
-    meta: { name: '分组柱状图图' },
+    meta: { name: '分组柱状图' },
     component: DodgeColumn,
+  },
+  {
+    path: 'bar/stack-column',
+    meta: { name: '层叠柱状图' },
+    component: StackColumn,
   },
 ];
---
+++ demo/pages/bar/stack-column.vue
@@ -0,0 +1,88 @@
+<template>
+  <vf-chart :data="data">
+    <vf-interval position="月份*月均降雨量" color="name" adjust="stack"/>
+    <vf-tooltip link-legend/>
+  </vf-chart>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [{
+        name: 'London',
+        月份: 'Jan.',
+        月均降雨量: 18.9,
+      }, {
+        name: 'London',
+        月份: 'Feb.',
+        月均降雨量: 28.8,
+      }, {
+        name: 'London',
+        月份: 'Mar.',
+        月均降雨量: 39.3,
+      }, {
+        name: 'London',
+        月份: 'Apr.',
+        月均降雨量: 81.4,
+      }, {
+        name: 'London',
+        月份: 'May.',
+        月均降雨量: 47,
+      }, {
+        name: 'London',
+        月份: 'Jun.',
+        月均降雨量: 20.3,
+      }, {
+        name: 'London',
+        月份: 'Jul.',
+        月均降雨量: 24,
+      }, {
+        name: 'London',
+        月份: 'Aug.',
+        月均降雨量: 35.6,
+      }, {
+        name: 'Berlin',
+        月份: 'Jan.',
+        月均降雨量: 12.4,
+      }, {
+        name: 'Berlin',
+        月份: 'Feb.',
+        月均降雨量: 23.2,
+      }, {
+        name: 'Berlin',
+        月份: 'Mar.',
+        月均降雨量: 34.5,
+      }, {
+        name: 'Berlin',
+        月份: 'Apr.',
+        月均降雨量: 99.7,
+      }, {
+        name: 'Berlin',
+        月份: 'May.',
+        月均降雨量: 52.6,
+      }, {
+        name: 'Berlin',
+        月份: 'Jun.',
+        月均降雨量: 35.5,
+      }, {
+        name: 'Berlin',
+        月份: 'Jul.',
+        月均降雨量: 37.4,
+      }, {
+        name: 'Berlin',
+        月份: 'Aug.',
+        月均降雨量: 42.4,
+      }],
+    };
+  },
+  methods: {
+    handleShow(ev) {
+      const items = ev.items;
+      items[0].name = null;
+      items[0].name = items[0].title;
+      items[0].value = `¥ ${items[0].value}`;
+    },
+  },
+};
+</script>
