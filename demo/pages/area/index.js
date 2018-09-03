449128698024122d33e37bce21608b52e7577df5
王自强 <814120507@qq.com>
2018-09-03 14:25:29 +0800

    添加area——区域图（存在空值）

--- demo/pages/area/index.js
+++ demo/pages/area/index.js
@@ -1,5 +1,6 @@
 import Basic from './basic';
 import WithNegative from './with-negative';
+import AreaNone from './area-none';
 
 export default [
   {
@@ -11,5 +12,10 @@ export default [
     path: 'area/with-negative',
     meta: { name: '带负值面积图' },
     component: WithNegative,
+  },
+  {
+    path: 'area/area-none',
+    meta: { name: '区域图（存在空值）' },
+    component: AreaNone,
   },
 ];
