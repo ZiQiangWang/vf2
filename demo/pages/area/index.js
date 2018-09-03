86418c074e248f6ceab69b880f99f4915dbcc153
王自强 <814120507@qq.com>
2018-09-03 14:34:04 +0800

    添加area——带负值面积图（x 基线不为 0）

--- demo/pages/area/index.js
+++ demo/pages/area/index.js
@@ -1,6 +1,7 @@
 import Basic from './basic';
 import WithNegative from './with-negative';
 import AreaNone from './area-none';
+import WithNegativeNotStartOnZero from './with-negative-not-start-on-zero';
 
 export default [
   {
@@ -17,5 +18,10 @@ export default [
     path: 'area/area-none',
     meta: { name: '区域图（存在空值）' },
     component: AreaNone,
+  },
+  {
+    path: 'area/with-negative-not-start-on-zero',
+    meta: { name: '带负值面积图（x 基线不为 0）' },
+    component: WithNegativeNotStartOnZero,
   },
 ];
---
+++ demo/pages/area/with-negative-not-start-on-zero.vue
@@ -0,0 +1,58 @@
+<template>
+  <vf-chart :data="data">
+    <vf-line position="month*value"/>
+    <vf-area position="month*value" :start-on-zero="false"/>
+    <vf-scale :options="{
+      month: {
+        range: [0, 1]
+      },
+      value: {
+        tickCount: 5
+      }
+    }"/>
+    <vf-tooltip :show-crosshairs="true"/>
+    <vf-axis field="value" :label="text =>{
+      const textCfg = {};
+      if (text <= 0) {
+        textCfg.fill = '#1CAA3D';
+        textCfg.fontWeight = 'bold';
+      }
+      return textCfg;
+    }"/>
+    <vf-axis field="month" justify-label-x/>
+  </vf-chart>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [{
+        month: 'Jan.',
+        value: 6.06,
+      }, {
+        month: 'Feb.',
+        value: 82.2,
+      }, {
+        month: 'Mar.',
+        value: -22.11,
+      }, {
+        month: 'Apr.',
+        value: 21.53,
+      }, {
+        month: 'May.',
+        value: -21.74,
+      }, {
+        month: 'Jun.',
+        value: 73.61,
+      }, {
+        month: 'Jul.',
+        value: 53.75,
+      }, {
+        month: 'Aug.',
+        value: 60.32,
+      }],
+    };
+  },
+};
+</script>
