014db8ccc35327a47a590e5b6bb73fa6ca5d76fc
王自强 <814120507@qq.com>
2018-09-04 21:31:58 +0800

    添加雷达面积图

---
+++ demo/pages/radar/area.vue
@@ -0,0 +1,100 @@
+<template>
+  <vf-chart :data="data">
+    <vf-line position="item*score" color="user"/>
+    <vf-area position="item*score" color="user"/>
+    <vf-point position="item*score" color="user" :chart-style="{
+      stroke: '#fff',
+      lineWidth: 1
+    }"/>
+    <vf-coord type="polar"/>
+    <vf-scale field="score" min="0" max="120" tick-count="4" :nice="false"/>
+    <vf-axis
+      field="score"
+      :label="(text, index, total) => {
+        if (index === total - 1) return null;
+        return {
+          top: true
+        };
+      }"
+      :grid="{
+        lineDash: null,
+        type: 'arc' // 弧线网格
+      }"/>
+    <vf-axis field="item" :grid="{lineDash: null}"/>
+    <vf-tooltip link-legend/>
+  </vf-chart>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [{
+        item: 'Design',
+        user: '用户 A',
+        score: 70,
+      }, {
+        item: 'Design',
+        user: '用户 B',
+        score: 30,
+      }, {
+        item: 'Development',
+        user: '用户 A',
+        score: 60,
+      }, {
+        item: 'Development',
+        user: '用户 B',
+        score: 70,
+      }, {
+        item: 'Marketing',
+        user: '用户 A',
+        score: 50,
+      }, {
+        item: 'Marketing',
+        user: '用户 B',
+        score: 60,
+      }, {
+        item: 'Users',
+        user: '用户 A',
+        score: 40,
+      }, {
+        item: 'Users',
+        user: '用户 B',
+        score: 50,
+      }, {
+        item: 'Test',
+        user: '用户 A',
+        score: 60,
+      }, {
+        item: 'Test',
+        user: '用户 B',
+        score: 70,
+      }, {
+        item: 'Language',
+        user: '用户 A',
+        score: 70,
+      }, {
+        item: 'Language',
+        user: '用户 B',
+        score: 50,
+      }, {
+        item: 'Technology',
+        user: '用户 A',
+        score: 70,
+      }, {
+        item: 'Technology',
+        user: '用户 B',
+        score: 40,
+      }, {
+        item: 'Support',
+        user: '用户 A',
+        score: 60,
+      }, {
+        item: 'Support',
+        user: '用户 B',
+        score: 40,
+      }],
+    };
+  },
+};
+</script>
--- demo/pages/radar/index.js
+++ demo/pages/radar/index.js
@@ -1,9 +1,15 @@
 const Basic = () => import('./basic');
+const Area = () => import('./area');
 
 export default [
   {
     path: 'radar/basic',
     meta: { name: '雷达图' },
     component: Basic,
+  },
+  {
+    path: 'radar/area',
+    meta: { name: '雷达面积图' },
+    component: Area,
   },
 ];
